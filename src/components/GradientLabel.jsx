febb58e37e22d0cd9f0b7f7a2d8aa4e84bf601b2
wqoh <wqoh@hitachi-ebworx.com>
2025-05-13 21:37:54 +0800

    solve the styling for LIVESTREAM

--- src/components/GradientLabel.jsx
+++ src/components/GradientLabel.jsx
@@ -3,11 +3,7 @@ import React from "react";
 const GradientLabel = ({ width, title }) => {
   return (
     <div
-<<<<<<< Updated upstream
-      className={`w-[${width}px] h-[35px] rounded-[8px] mb-4 flex justify-start items-center ${["ANALYTICS", "LIVESTREAM"].includes(title) ? "px-4" : "pl-4"} font-inter text-[18px] font-semibold relative overflow-hidden bg-[rgba(74,197,223,0.15)]`}
-=======
       className={`w-[${width}px] h-[35px] rounded-[8px] ${title === "LIVESTREAM" ? "mb-0" : "mb-4"} flex justify-start items-center px-4 font-inter text-[18px] font-semibold relative overflow-hidden bg-[rgba(74,197,223,0.15)]`}
->>>>>>> Stashed changes
     >
       <span className="bg-clip-text text-transparent bg-gradient-to-r from-[#11CCA8] to-[#4AC5DF] z-10">
         {title}
